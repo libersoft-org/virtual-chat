ffeb539f09c493fe4e42ca72295e56fc93e58bf4
Liberland Software Foundation <info@libersoft.org>
2023-12-31 23:38:23 +0100

    First commit

--- src/libs/socket.js
+++ src/libs/socket.js
@@ -2,31 +2,41 @@ const { Common } = require('./common.js');
 
 class Socket {
  constructor() {
-  this.sockets = {};
+  this.connections = {};
+  this.users = {};
  }
 
  onOpen(ws) {
   const uuid = crypto.randomUUID();
   Common.addLog('WS new connection: ' + uuid);
   ws.uuid = uuid;
-  this.sockets[uuid] = ws;
-  Common.addLog('WS connections: ' + Object.keys(this.sockets).length);
+  this.connections[uuid] = ws;
+  Common.addLog('WS connections: ' + Object.keys(this.connections).length);
  }
 
  onClose(ws, message) {
   Common.addLog('WS connection closed: ' + ws.uuid);
-  delete this.sockets[ws.uuid];
-  Common.addLog('WS connections: ' + Object.keys(this.sockets).length);
+  delete this.connections[ws.uuid];
+  Common.addLog('WS connections: ' + Object.keys(this.connections).length);
  }
- 
+
  onMessage(ws, json) {
   try {
    const msg = JSON.parse(json);
    console.log(ws.uuid, msg);
    if ('method' in msg && 'data' in msg) {
     switch(msg.method) {
+     case 'enter':
+      this.getEnter(ws, msg.data);
+      break;
+     case 'exit':
+      this.getExit(ws);
+      break;
      case 'move':
       this.getMove(ws, msg.data);
+      break;
+     case 'message':
+      this.getMessage(ws, msg.data);
       break;
      default:
       this.send(ws, { error: 3, message: 'Unknown method in command' });
@@ -44,25 +54,50 @@ class Socket {
  }
 
  broadcast(obj) {
-  console.log('BROADCASTING');
-  Common.addLog('WS CONNECTIONS: ' + Object.keys(this.sockets).length);
-  for(const ws of Object.values(this.sockets)) {
-   console.log('Sending to:', ws.uuid);
-   ws.send(JSON.stringify(obj));
-  }
+  for(const ws of Object.values(this.connections)) ws.send(JSON.stringify(obj));
+ }
+
+ getEnter(ws, data) {
+  // TODO: check if not already in room
+  if (!'name' in data) return this.send(ws, { method: 'enter', error: 2, message: 'Missing name' });
+  // TODO: check name validaty
+  if (!'color' in data) return this.send(ws, { method: 'enter', error: 4, message: 'Missing color' });
+  if (!Number.isInteger(data.color) || number < 1 || number > 6) return this.send(ws, { method: 'enter', error: 5, message: 'Wrong color ID' });
+  if (!'sex' in data) return this.send(ws, { method: 'enter', error: 6, message: 'Missing sex' });
+  if (!Number.isInteger(data.sex) || number < 0 || number > 4) return this.send(ws, { method: 'enter', error: 7, message: 'Wrong sex ID' });
+  // TODO: enter 
+ }
+
+ getExit(ws) {
+  // TODO: check if in room - if not, throw an error
+  // TODO: exit room
  }
 
  getMove(ws, data) {
-  if ('x' in data && 'y' in data) {
-   this.broadcast({
-    method: 'move',
-    data: {
-     user: ws.uuid,
-     x: data.x,
-     y: data.y
-    }
-   });
-  } else this.send(ws, { error: 4, message: 'Missing coordinates' });
+  // TODO: check if in room - if not, throw an error
+  if ('x' in data && 'y' in data) return this.send(ws, { method: 'move', error: 2, message: 'Missing coordinates' });
+  this.broadcast({
+   method: 'move',
+   data: {
+    user: ws.uuid,
+    x: data.x,
+    y: data.y
+   }
+  });
+ }
+
+ getMessage(ws, data) {
+  // TODO: check if in room - if not, throw an error
+  if (!'message' in data) return this.send(ws, { method: 'message', error: 2, message: 'Missing message' });
+  // TODO: check if ID exists and then set a name
+  const name = 'User';
+  this.broadcast({
+   method: 'message',
+   data: {
+    name: name,
+    message: data.message,
+   }
+  });
  }
 }
 

bfd7bd4f254812a66c807759a36c4e6bb544f69f
LiberSoft <info@libersoft.org>
2026-03-08 16:27:16 +0100

    Cleanup

---
+++ frontend/src/lib/session.ts
@@ -0,0 +1,69 @@
+import { Network } from './network.ts';
+import { World } from './world.ts';
+import { chatMessages, isLoggedIn } from './stores.ts';
+import type { EnterData, LeaveData, MoveData, MessageData, UsersEntry, ExpressionData } from '@shared/protocol.ts';
+
+export interface Session {
+	enter: (name: string, sex: boolean | null, color: number) => void;
+	leave: () => void;
+	sendMessage: (text: string) => void;
+	setExpression: (expression: number) => void;
+}
+
+export function createSession(container: HTMLElement, wsUrl: string): Session {
+	const network = new Network(wsUrl, {
+		onEnter: (data: EnterData) => {
+			network.myUuid = data.uuid;
+			isLoggedIn.set(true);
+			world.getUser(data.name, data.color, data.sex, data.x, data.y, data.angle);
+			world.createLabel(data.name);
+			network.sendUsers();
+		},
+		onUserEntered: (data: EnterData) => {
+			world.addOtherPlayer(data.uuid, data.name, data.color, data.x, data.y, data.angle, data.expression);
+		},
+		onLeave: (data: LeaveData) => {
+			if (data.uuid === network.myUuid) {
+				world.removeUser();
+				network.myUuid = undefined;
+				isLoggedIn.set(false);
+			} else {
+				world.removeOtherPlayer(data.uuid);
+			}
+		},
+		onMove: (data: MoveData) => {
+			world.moveOtherPlayer(data.user, data.x, data.y, data.angle);
+		},
+		onMessage: (data: MessageData) => {
+			chatMessages.update(msgs => [...msgs, { name: data.name, message: data.message }]);
+			if (data.user === network.myUuid) {
+				if (world.user) world.createChatBubble(data.message, world.user);
+			} else {
+				const player = world.otherPlayers.get(data.user);
+				if (player) world.createChatBubble(data.message, player.group);
+			}
+		},
+		onUsers: (data: UsersEntry[]) => {
+			for (const entry of data) {
+				if (entry.uuid !== network.myUuid) {
+					world.addOtherPlayer(entry.uuid, entry.user.name, entry.user.color, entry.user.x, entry.user.y, entry.user.angle, entry.user.expression);
+				}
+			}
+		},
+		onExpression: (data: ExpressionData) => {
+			if (data.user === network.myUuid) world.setExpression(data.expression);
+			else world.setOtherPlayerExpression(data.user, data.expression);
+		},
+	});
+
+	const world = new World(container, (x: number, y: number, angle: number) => {
+		network.sendMove(x, y, angle);
+	});
+
+	return {
+		enter: (name, sex, color) => network.sendEnter(name, sex, color),
+		leave: () => network.sendLeave(),
+		sendMessage: (text) => network.sendMessage(text),
+		setExpression: (expression) => network.sendExpression(expression),
+	};
+}
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import { Network } from '$lib/network';
-	import { World } from '$lib/world';
-	import { chatMessages, isLoggedIn } from '$lib/stores';
-	import type { EnterData, LeaveData, MoveData, MessageData, UsersEntry, ExpressionData } from '@shared/protocol.ts';
+	import { createSession, type Session } from '$lib/session';
+	import { isLoggedIn } from '$lib/stores';
 	import LoginForm from '../components/LoginForm.svelte';
 	import ChatWindow from '../components/ChatWindow.svelte';
 	import MessageInput from '../components/MessageInput.svelte';
@@ -14,76 +12,12 @@
 	import DebugPanel from '../components/DebugPanel.svelte';
 	import ExpressionPicker from '../components/ExpressionPicker.svelte';
 	let container: HTMLDivElement;
-	let world: World;
-	let network: Network;
+	let session: Session;
 	const wsUrl = import.meta.env['VITE_BACKEND_URL'] || (import.meta.env.DEV ? `${location.protocol === 'https:' ? 'wss' : 'ws'}://${location.hostname}:7010` : `${location.protocol === 'https:' ? 'wss' : 'ws'}://${location.host}`);
 
 	onMount(() => {
-		world = new World(container, (x: number, y: number, angle: number) => {
-			network.sendMove(x, y, angle);
-		});
-
-		network = new Network(wsUrl, {
-			onEnter: (data: EnterData) => {
-				network.myUuid = data.uuid;
-				isLoggedIn.set(true);
-				world.getUser(data.name, data.color, data.sex, data.x, data.y, data.angle);
-				world.createLabel(data.name);
-				network.sendUsers();
-			},
-			onUserEntered: (data: EnterData) => {
-				world.addOtherPlayer(data.uuid, data.name, data.color, data.x, data.y, data.angle, data.expression);
-			},
-			onLeave: (data: LeaveData) => {
-				if (data.uuid === network.myUuid) {
-					world.removeUser();
-					network.myUuid = undefined;
-					isLoggedIn.set(false);
-				} else {
-					world.removeOtherPlayer(data.uuid);
-				}
-			},
-			onMove: (data: MoveData) => {
-				world.moveOtherPlayer(data.user, data.x, data.y, data.angle);
-			},
-			onMessage: (data: MessageData) => {
-				chatMessages.update(msgs => [...msgs, { name: data.name, message: data.message }]);
-				if (data.user === network.myUuid) {
-					if (world.user) world.createChatBubble(data.message, world.user);
-				} else {
-					const player = world.otherPlayers.get(data.user);
-					if (player) world.createChatBubble(data.message, player.group);
-				}
-			},
-			onUsers: (data: UsersEntry[]) => {
-				for (const entry of data) {
-					if (entry.uuid !== network.myUuid) {
-						world.addOtherPlayer(entry.uuid, entry.user.name, entry.user.color, entry.user.x, entry.user.y, entry.user.angle, entry.user.expression);
-					}
-				}
-			},
-			onExpression: (data: ExpressionData) => {
-				if (data.user === network.myUuid) world.setExpression(data.expression);
-				else world.setOtherPlayerExpression(data.user, data.expression);
-			},
-		});
+		session = createSession(container, wsUrl);
 	});
-
-	function handleEnter(name: string, sex: boolean | null, color: number) {
-		network.sendEnter(name, sex, color);
-	}
-
-	function handleLeave() {
-		network.sendLeave();
-	}
-
-	function handleSendMessage(text: string) {
-		network.sendMessage(text);
-	}
-
-	function handleExpression(expression: number) {
-		network.sendExpression(expression);
-	}
 </script>
 
 <style>
@@ -99,13 +33,13 @@
 <div id="world-container" bind:this={container}></div>
 
 {#if !$isLoggedIn}
-	<LoginForm onenter={handleEnter} />
+	<LoginForm onenter={session.enter} />
 {:else}
 	<ChatWindow />
 	<FpsCounter />
-	<MessageInput onsend={handleSendMessage} />
-	<LeaveButton onleave={handleLeave} />
-	<ExpressionPicker onpick={handleExpression} />
+	<MessageInput onsend={session.sendMessage} />
+	<LeaveButton onleave={session.leave} />
+	<ExpressionPicker onpick={session.setExpression} />
 {/if}
 
 <DebugPanel />
